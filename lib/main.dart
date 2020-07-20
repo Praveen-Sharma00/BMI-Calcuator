1e1201ce82c2cf9a1527623e621bd8d95f024a18
Praveen-Sharma00 <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-07-20 20:25:07 +0530

    Added Dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,13 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.white),
+        ),
+      ),
       home: InputPage(),
     );
   }
@@ -25,9 +32,6 @@ class _InputPageState extends State<InputPage> {
       ),
       body: Center(
         child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
       ),
     );
   }
